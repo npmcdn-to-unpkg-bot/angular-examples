86a4afb5c0014f866649a10aad66c1051a1d3aa6
Alex Greco <grecoa66@students.rowan.edu>
2016-06-09 17:09:13 -0400

    working demo at this point

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 var app = angular.module("ToDoList", []);
 
-app.controller("ListController", function(){
+app.controller("ListController", function($log){
 
-	this.items = ["Learn angularjs.", "Send status report to Hansika.", "Remember the asparagus in the refridgerator."];
-	this.task1 = "learn how to use controllers.";
+	var listCtrl = this;
+
+	listCtrl.items = ["Learn angularjs.", "Send status report to Hansika.", "Remember the asparagus in the refridgerator."];
+
+	//remove parameter from items
+	listCtrl.removeItem = function(item){
+		//check if item is null
+		if(item === null){
+			listCtrl.$log(item);
+		}else{
+			//find item in array and remove it.
+			for(var i = 0; i < listCtrl.items.length; i++){
+				if(item === listCtrl.items[i]){
+					listCtrl.items.splice(i, 1);
+					console.log("Task removed: ", )
+				}
+			}
+		}
+	}; //end removeItem
+
+	//push new item into items
+	listCtrl.addItem = function(){
+		if(listCtrl.newToDo == null || listCtrl.newToDo === ""){
+			alert("Your task is empty!");
+		}else{
+			listCtrl.items.push(listCtrl.newToDo);
+			console.log("new task: ", listCtrl.newToDo)
+		}
+	};//end addItem
+
 });
 app.controller('MainCtrl', function () {
   this.title = 'Some title';
+
+
 });
