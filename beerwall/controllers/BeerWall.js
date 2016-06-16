298e652e1e3619ac19c9e0c01c3ef3d1987aa22e
Alex Greco <grecoa66@students.rowan.edu>
2016-06-15 22:05:08 -0400

    added beer wall proj

---
+++ beerwall/controllers/BeerWall.js
@@ -0,0 +1,8 @@
+var app = angular.module("BeerWall", []);
+
+app.controller("wallController", function(){
+
+	var vm = this;
+
+	vm.labels = ["labels/kona-longboard.jpg", "labels/dogfish-75-minute.jpg"];
+});
